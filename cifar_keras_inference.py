0ab3c4252cf07cd2cf6574ef8c2d5d7786c8e3f9
Shanshan Xie <sxie@cerc.utexas.edu>
2020-08-31 16:02:10 -0500

    bad accuracy

--- cifar_keras_inference.py
+++ cifar_keras_inference.py
@@ -97,8 +97,8 @@ X_test_padded[:X_test.shape[0], :X_test.shape[1], 1:X_test.shape[2]+1, 1:X_test.
 # print(X_test_padded[:1].shape, 'test samples')
 # print(Y_test[:1].shape, 'test samples values')
 
-X_test_small = X_test_padded[:10]
-Y_test_small = Y_test[:10]
+X_test_small = X_test_padded[:2]
+Y_test_small = Y_test[:2]
 
 # Weight
 weight_data_conv1 = []
@@ -164,15 +164,15 @@ def network(X_test_data, Y_test_data):
     #                  name='scaling1'))  ## Dividing by 27 (MAV) and 18.296 (Instead of 128), so need to multiply by factor of 7 in gain stage
     # model.add(Lambda(lambda x: K.clip(x, clip1_min, clip1_max), name='clip1'))
     # model.add(Activation(relu_layer, name='act_conv1'))
-    #
-    # # Conv2, Scaling2 and ReLU2
-    # model.add(
-    #     Conv2D(32, kernel_size=(3, 3), data_format='channels_first', kernel_initializer='he_normal', padding='same',
-    #            use_bias=use_bias, name='conv2'))
-    # model.add(Lambda(lambda x: floor_func(x, conv_scale[1]),
-    #                  name='scaling2'))  ## Dividing by 288 (MAV) and 1 (Instead of 128), so need to multiply by factor of 128 in gain stage
-    # model.add(Lambda(lambda x: K.clip(x, clip2_min, clip2_max), name='clip2'))
-    model.add(Activation(relu_layer, name='act_conv2', input_shape=(32,32,32)))
+
+    # Conv2, Scaling2 and ReLU2
+    model.add(
+        Conv2D(32, kernel_size=(3, 3), input_shape=(32,32,32), data_format='channels_first', kernel_initializer='he_normal', padding='same',
+               use_bias=use_bias, name='conv2'))
+    model.add(Lambda(lambda x: floor_func(x, conv_scale[1]),
+                     name='scaling2'))  ## Dividing by 288 (MAV) and 1 (Instead of 128), so need to multiply by factor of 128 in gain stage
+    model.add(Lambda(lambda x: K.clip(x, clip2_min, clip2_max), name='clip2'))
+    model.add(Activation(relu_layer, name='act_conv2'))
 
     # Pool1
     model.add(MaxPooling2D(pool_size=(2, 2), name='pool1', data_format='channels_first'))
@@ -355,21 +355,22 @@ def network_f(X_test_data, Y_test_data):
 # testing
 def __main__():
     # print('nothing here')
-    next_layer_input = conv(X_test_small, weight_data_conv1, 32)
-
+    next_layer_input = conv(X_test_small, weight_data_conv1, 22)
+    print('shape = ' + str(next_layer_input.shape))
+    print('max = ' + str(np.amax(next_layer_input)))
     # padding for conv2
     next_layer_input_padding = np.zeros(shape=(
     next_layer_input.shape[0], next_layer_input.shape[1], next_layer_input.shape[2] + 2, next_layer_input.shape[3] + 2))
     next_layer_input_padding[:next_layer_input.shape[0], :next_layer_input.shape[1], 1:next_layer_input.shape[2] + 1,
     1:next_layer_input.shape[3] + 1] = next_layer_input
 
-    next_layer_input_con2 = conv(next_layer_input_padding, weight_data_conv2, 64)
-    print('conv2 shape = ' + str(next_layer_input_con2.shape))
-
+    # next_layer_input_con2 = conv(next_layer_input_padding, weight_data_conv2, 128)
+    # print('conv2 shape = ' + str(next_layer_input_con2.shape))
+    # print('conv2 max = ' + str(np.amax(next_layer_input_con2)))
     # print(next_layer_input)
     # print('shape = ' + str(next_layer_input.shape))  np.zeros(shape=(1,32,32,32))
 
-    network(X_test_data=next_layer_input_con2, Y_test_data=Y_test_small)
+    network(X_test_data=next_layer_input, Y_test_data=Y_test_small)
 
     # print(X_test_small.shape, 'test samples', Y_test_small.shape, 'test_sample_value')
     # print('shape = ' + str(next_layer_input.shape))
